9214a712e9e2ff36e866395bb64d07ad415eb59d
oliversaternus <mail@oliversaternus.de>
2020-01-20 13:24:02 +0100

    spaces

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,20 +9,23 @@ const styles: any = {
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
+    backgroundColor: '#e8eff3'
   },
   row: {
     display: 'flex',
     flexDirection: 'row',
-    width: 'calc(100% - 2px)',
-    height: 'calc(100% / 9)'
+    width: '100%',
+    height: 'calc((100% - 5px) / 9)'
   },
   cell: {
-    width: 'calc(100% / 9)',
-    height: '100%',
-    border: '1px solid black',
+    width: 'calc((100% - 5px) / 9 - 1px)',
+    height: 'calc(100% - 1px)',
+    margin: 0.5,
+    border: '0px solid black',
     color: 'black',
     transition: 'all 0.225s linear',
+    backgroundColor: 'white',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -33,6 +36,10 @@ const styles: any = {
   },
   fixed: {
     cursor: 'auto'
+  },
+  square: {
+    backgroundColor: 'black',
+    boxShadow: '0px 0px 12px rgba(0,0,0,0.7)'
   }
 };
 
@@ -54,7 +61,7 @@ const setField = (row: number, col: number, value: number): number[][] => {
 
 const Square: React.FC<{ size: number }> = (props) => {
   return (
-    <div style={{ width: props.size, height: props.size }}>
+    <div style={{ ...styles.square, width: props.size - 2, height: props.size - 2, padding: 1, paddingRight: 0.5, paddingLeft: 1.5 }}>
       {props.children}
     </div>);
 }
@@ -76,22 +83,26 @@ const Home: React.FC = () => {
           <IonTitle>Sudoku</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
+      <IonContent>
         <div style={styles.container}>
           <Square size={maxSize}>
-            {fields.map((row, rowIndex: number) =>
-              <div key={rowIndex} style={styles.row}>{
-                row.map((cell, colIndex: number) => {
-                  const isFixed = game.initial.values[rowIndex][colIndex] !== 0;
-                  const isSelected = rowIndex === selected[0] && colIndex === selected[1];
-                  let cellStyle = isSelected ? { ...styles.cell, ...styles.selected } : styles.cell;
-                  cellStyle = isFixed ? { ...cellStyle, ...styles.fixed } : cellStyle;
-                  return (
-                    <div key={colIndex} style={{ ...cellStyle, fontSize: maxSize / 16 }} onClick={() => !isFixed && setSelected([rowIndex, colIndex])}>
-                      {cell || ''}
-                    </div>)
-                })
-              }</div>)}
+            {fields.map((row, rowIndex: number) => {
+              const rowStyle = rowIndex && (rowIndex % 3 === 0) ? { ...styles.row, marginTop: 2.5 } : styles.row;
+              return (
+                <div key={rowIndex} style={rowStyle}>{
+                  row.map((cell, colIndex: number) => {
+                    const isFixed = game.initial.values[rowIndex][colIndex] !== 0;
+                    const isSelected = rowIndex === selected[0] && colIndex === selected[1];
+                    let cellStyle = isSelected ? { ...styles.cell, ...styles.selected } : styles.cell;
+                    cellStyle = isFixed ? { ...cellStyle, ...styles.fixed } : cellStyle;
+                    cellStyle = colIndex && (colIndex % 3 === 0) ? { ...cellStyle, marginLeft: 2.5 } : cellStyle;
+                    return (
+                      <div key={colIndex} style={{ ...cellStyle, fontSize: maxSize / 16 }} onClick={() => !isFixed && setSelected([rowIndex, colIndex])}>
+                        {cell || ''}
+                      </div>)
+                  })}
+                </div>)
+            })}
           </Square>
         </div>
       </IonContent>
